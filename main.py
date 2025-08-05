93eb8d4c1c09d7b3fd565938e30c742fce6c171e
olegmir59 <udom1@ya.ru>
2025-08-05 15:17:17 +0700

    Создать класс Game и его методы

--- main.py
+++ main.py
@@ -12,3 +12,30 @@ class Hero:
     def is_alive(self):
         """Проверяет, жив ли герой (здоровье > 0)"""
         return self.health > 0
+
+
+class Game:
+    def __init__(self, player_name):
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
+
+    def start(self):
+        print("Игра началась!")
+        round_number = 1
+
+        while self.player.is_alive() and self.computer.is_alive():
+            print(f"\nРаунд {round_number}")
+            # Ход игрока
+            self.player.attack(self.computer)
+
+            if not self.computer.is_alive():
+                print(f"{self.computer.name} погиб. Победил {self.player.name}!")
+                break
+
+            # Ход компьютера
+            self.computer.attack(self.player)
+
+            if not self.player.is_alive():
+                print(f"{self.player.name} погиб. Победил {self.computer.name}!")
+
+            round_number += 1
